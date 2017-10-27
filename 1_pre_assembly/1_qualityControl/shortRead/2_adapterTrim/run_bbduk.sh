23770e51d2095d953e24a4269d2ab325bd6b3efe
weiwen wang <u4987172@anu.edu.au>
2017-10-28 04:08:30 +1100

    Update run_bbduk.sh

--- 1_pre_assembly/1_qualityControl/shortRead/2_adapterTrim/run_bbduk.sh
+++ 1_pre_assembly/1_qualityControl/shortRead/2_adapterTrim/run_bbduk.sh
@@ -24,12 +24,12 @@ for in1 in $(find $inputf -name "*R.fastq.gz"); do
     echo $in1
     echo $in2
 
-    f1=$(basename ${in1%%R1.fastq.gz}"R1.trimmed.fastq.gz")
-    f2=$(basename ${in1%%R1.fastq.gz}"R2.trimmed.fastq.gz")
+    f1=$(basename ${in1%%R1.fastq.gz}"R1.trim.fastq.gz")
+    f2=$(basename ${in1%%R1.fastq.gz}"R2.trim.fastq.gz")
 
     out1=$outputtrimreads/$f1
     out2=$outputtrimreads/$f2
-    sampleid=$outputtrimreads${f1%%R1.trimmed.fastq.gz}
+    sampleid=$outputtrimreads${f1%%R1.trim.fastq.gz}
 
     bbduk.sh in1=$in1 in2=$in2 out1=$out1 out2=$out2 minlen=$minlen k=25 mink=8 ktrim=r ref=$adaptors hdist=1 overwrite=f qtrim=rl trimq=$trimq t=$threads bhist=$sampleid"bhist.txt" qhist=$sampleid"qhist.txt" gchist=$sampleid"gchist.txt" aqhist=$sampleid"aqhist.txt" lhist=$sampleid"lhist.txt" > $sampleid"bbduk_log.txt"
 

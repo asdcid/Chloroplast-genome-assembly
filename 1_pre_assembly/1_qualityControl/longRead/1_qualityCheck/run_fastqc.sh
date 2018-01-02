fa0abab973c1786d6ca72d5b1c7b92e3834cfd30
weiwen wang <u4987172@anu.edu.au>
2018-01-02 15:09:31 +1100

    Update run_fastqc.sh

--- 1_pre_assembly/1_qualityControl/longRead/1_qualityCheck/run_fastqc.sh
+++ 1_pre_assembly/1_qualityControl/longRead/1_qualityCheck/run_fastqc.sh
@@ -3,13 +3,10 @@
 
 export PATH='fastqcPath':$PATH
 
-#inputFile='/home/raymond/work/Eucalyptus_pauciflora/genome/data/nanopore/RB7_C4.fastq.gz'
-#outputDir='result/'
-#threads=20
+inputFile='/home/raymond/work/Eucalyptus_pauciflora/genome/data/nanopore/RB7_C4.fastq.gz'
+outputDir='result/'
+threads=20
 
-inpuFile=$1
-outputDir=$2
-threads=$3
 
 fastqc $inputFile -o $outputDir -t $threads
 

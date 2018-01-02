ac7553fb5bc4455010a2a719532df808c1f2f38e
weiwen wang <u4987172@anu.edu.au>
2018-01-03 01:04:32 +1100

    Update run_unicycler.sh

--- 2_assembly/hybrid/run_unicycler.sh
+++ 2_assembly/hybrid/run_unicycler.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 
-#export PATH='/home/raymond/devel/python3/install/bin/':$PATH
-#export PATH='/home/raymond/devel/unicycler/Unicycler_old/':$PATH
-#export PATH='/home/raymond/devel/unicycler/three_party/SPAdes/SPAdes-3.10.1-Linux/bin/':$PATH
-#export PATH='/home/raymond/devel/polish/pilon/':$PATH
+export PATH='/home/raymond/devel/python3/install/bin/':$PATH
+export PATH='/home/raymond/devel/unicycler/Unicycler_old/':$PATH
+export PATH='/home/raymond/devel/unicycler/three_party/SPAdes/SPAdes-3.10.1-Linux/bin/':$PATH
+export PATH='/home/raymond/devel/polish/pilon/':$PATH
 
-export PATH='unicycler/path':$PATH
-export PATH='SPAdes/path':$PATH
-export PATH='pilon/path':$PATH
+
 
 inputFiles_longRead=$1
 inputFile_R1=$2

c62b4bfa2fc684dade39e06f9e7b7cbeabe7903e
weiwen wang <u4987172@anu.edu.au>
2018-01-02 15:28:24 +1100

    Update run_canu.sh

--- 2_assembly/longReadOnly/canu/run_canu.sh
+++ 2_assembly/longReadOnly/canu/run_canu.sh
@@ -2,26 +2,23 @@
 
 set -e
 
-#export PATH='/home/raymond/devel/canu/canu/Linux-amd64/bin/':$PATH
+export PATH='/home/raymond/devel/canu/canu/Linux-amd64/bin/':$PATH
 
-export PATH='canu/path':$PATH
 
 inputFile=$1
 outputDir=$2
-name=$3
-genomeSize=$4
-corOutCoverage=$5
-correctedErrorRate=$6
-threads=$7
-#/home/raymond/devel/gnuplot/install/bin/gnuplot
-gnuplotPath=$8
+name='Epau'
+genomeSize=160kb
+#If the coverage is lower than 40x, the correctedErrorRate is set to 0.154
+correctedErrorRate=0.144
+threads=40
+gnuplotPath=/home/raymond/devel/gnuplot/install/bin/gnuplot
 echo $inputFile
 echo $outputDir
 
 canu \
     -p $name \
     -d $outputDir \
-    corOutCoverage=$corOutCoverage \
     correctedErrorRate=$correctedErrorRate \
     genomesize=$genomeSize \
     -nanopore-raw $inputFile\

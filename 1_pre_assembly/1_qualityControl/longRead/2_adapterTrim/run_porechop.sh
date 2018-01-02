20b031beab46583b3ce084bc3299e88070bfbee1
weiwen wang <u4987172@anu.edu.au>
2018-01-02 14:35:43 +1100

    Update run_porechop.sh

--- 1_pre_assembly/1_qualityControl/longRead/2_adapterTrim/run_porechop.sh
+++ 1_pre_assembly/1_qualityControl/longRead/2_adapterTrim/run_porechop.sh
@@ -5,18 +5,15 @@ ulimit -c unlimited
 
 set -e
 
-#export PATH='/home/raymond/devel/python3/install/bin/':$PATH
-#export PATH='/home/raymond/devel/porechop/Porechop/':$PATH
+export PATH='/home/raymond/devel/python3/install/bin/':$PATH
+export PATH='/home/raymond/devel/porechop/Porechop/':$PATH
 
-export PATH='porchop/path':$PATH
 
-#inputDir='/home/raymond/work/Eucalyptus_pauciflora/genome/data/nanopore/'
-#outputDir='result'
-#threads=20
+inputDir='/home/raymond/work/Eucalyptus_pauciflora/genome/data/nanopore/'
+outputDir='result'
+threads=20
 
-inputDir=$1
-outputDir=$2
-threads=$3
+
 
 
 for inputFile in $inputDir/*fastq.gz

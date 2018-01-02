a07e58ee71e7ceab3196ac79b6f974a34494ead8
weiwen wang <u4987172@anu.edu.au>
2018-01-03 01:05:32 +1100

    Create run_unicycler_noSPAdes.sh

---
+++ 2_assembly/hybrid/run_unicycler_noSPAdes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+export PATH='/home/raymond/devel/python3/install/bin/':$PATH
+export PATH='/home/raymond/devel/unicycler/Unicycler_old/':$PATH
+export PATH='/home/raymond/devel/unicycler/three_party/SPAdes/SPAdes-3.10.1-Linux/bin/':$PATH
+export PATH='/home/raymond/devel/polish/pilon/':$PATH
+
+
+
+inputFiles_longRead=$1
+inputFile_R1=$2
+inputFile_R2=$3
+outputDir=$4
+threads=$5
+start=$(date +%s)
+
+unicycler-runner.py \
+    -1 $inputFile_R1 \
+    -2 $inputFile_R2 \
+    -l $inputFiles_longRead \
+    --no_correct \
+    -o $outputDir \
+    -t $threads
+
+end=$(date +%s)
+DIFF=$(( $end - $start ))
+echo $outputDir/$id "Excution time: " $DIFF " seconds." >> runTime.log

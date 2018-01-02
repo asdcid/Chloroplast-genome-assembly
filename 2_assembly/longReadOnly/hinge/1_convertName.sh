dc4e078f89cf2fd9f89f5c031f093ba0a8470892
weiwen wang <u4987172@anu.edu.au>
2018-01-02 15:29:51 +1100

    Update 1_convertName.sh

--- 2_assembly/longReadOnly/hinge/1_convertName.sh
+++ 2_assembly/longReadOnly/hinge/1_convertName.sh
@@ -2,13 +2,12 @@
 
 
 #from normal name to PabBio specific
-
-#inputDir='/home/raymond/work/Eucalyptus_pauciflora/genome/bin/assembly/hinge/data/'
-#outputDir='data/pacbioName'
-
 ##the file in inputDir should be fasta, fasta.gz format has not tested
-inputDir=$1
-outputDir=$2
+
+inputDir='/home/raymond/work/Eucalyptus_pauciflora/genome/bin/assembly/hinge/data/'
+outputDir='data/pacbioName'
+
+
 
 cd $outputDir
 for inputFile in $inputDir/*.fasta
